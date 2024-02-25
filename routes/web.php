8cfe907c158c44a4cc854333943cdd81a50edfae
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-02-25 14:18:54 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PenjualanController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'home']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage'])->name('food-beverage');
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth'])->name('beauty-health');
+    Route::get('/home-care', [ProductController::class, 'homeCare'])->name('home-care');
+    Route::get('/baby-kid', [ProductController::class, 'babyKid'])->name('baby-kid');
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+Route::get('/penjualan', [PenjualanController::class, 'show']);
